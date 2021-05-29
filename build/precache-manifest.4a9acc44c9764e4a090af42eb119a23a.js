c4c847272ce1c560d5bd343f13f8849336ee737e
tman5 <10875976+tman5@users.noreply.github.com>
2021-05-29 10:34:39 -0400

    Add files via upload

---
+++ build/precache-manifest.4a9acc44c9764e4a090af42eb119a23a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "992d03160dd301e7a008",
+    "url": "/static/js/main.992d0316.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "992d03160dd301e7a008",
+    "url": "/static/css/main.ec61523f.chunk.css"
+  },
+  {
+    "revision": "a0cb39818834240246d756190575d779",
+    "url": "/index.html"
+  }
+];
